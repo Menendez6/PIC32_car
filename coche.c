396dfebe9a92c4eef7dc6c7edf25b094550ef61f
Menendez6 <pabloj.menendez@gmail.com>
2022-04-24 13:54:50 +0200

    Coche terminado

--- coche.c
+++ coche.c
@@ -10,8 +10,10 @@
 # define MOTOR_B_PWM 7
 # define MOTOR_A_DIR 9
 # define MOTOR_A_PWM 8
+#define PERIODO 249
 
 int conexion, inter = 0;
+float factor_servicio;
 
 
 __attribute__((vector(8),interrupt(IPL2SOFT),nomips16))
@@ -44,46 +46,109 @@ void inicializarCoche(){
     //LATA = 0xFFFF;
     //PORTB = 0xFFFF;
     
+    SYSKEY=0xAA996655;
+    SYSKEY=0x556699AA;
+    RPC7R = 5; // OC1 conectado a RC7
+    RPC8R = 5;
+    SYSKEY=0x1CA11CA1;
+    
+    OC1CON = 0;
+    OC1R = 0;
+    OC1RS = 0;
+    OC1CON = 0x800E;
+    
+    OC2CON = 0;
+    OC2R = 0;
+    OC2RS = 0;
+    OC2CON = 0x800E;
+    
     T2CON = 0x0060; // OFF, preescalado 1:64
-    PR2 = 39062; // Tiempo = 50 us
+    PR2 = 39062; // Tiempo = 500 ms
     TMR2 = 0; // Inicializar la cuenta
     IFS0bits.T2IF = 0; // Borrar la bandera
     IEC0bits.T2IE = 1; // Habilitar interrupciones
     IPC2bits.T2IP = 2; // Prioridad interrupciones
-    T2CONbits.ON = 1; // Encender el Timer 3
+    T2CONbits.ON = 1; // Encender el Timer 2
+    
+    
+    
 }
 
 void modificarCoche(char c, float distancia){
     if (c!='\0'){
         //TMR2 = 0
         conexion = 1;
+        if (c == '0'){
+            factor_servicio = 0;
+        }
+        else if (c=='1'){
+            factor_servicio = 0.1;
+        }
+        else if (c=='2'){
+            factor_servicio = 0.2;
+        }
+        else if (c=='3'){
+            factor_servicio = 0.3;
+        }
+        else if (c=='4'){
+            factor_servicio = 0.4;
+        }
+        else if (c=='5'){
+            factor_servicio = 0.5;
+        }
+        else if (c=='6'){
+            factor_servicio = 0.6;
+        }
+        else if (c=='7'){
+            factor_servicio = 0.7;
+        }
+        else if (c=='8'){
+            factor_servicio = 0.8;
+        }
+        else if (c=='9'){
+            factor_servicio = 0.9;
+        }
+        else if (c=='q'){
+            factor_servicio = 1;
+        }
+        
         if(c == 'F'){  //move forward(all motors rotate in forward direction)
             if (distancia > 25.){
-                LATC |= (1<<MOTOR_A_PWM);
-                LATC |= (1<<MOTOR_B_PWM);
+                OC1RS = PERIODO * factor_servicio;
+                OC2RS = PERIODO * factor_servicio;
+                //LATC |= (1<<MOTOR_A_PWM);
+                //LATC |= (1<<MOTOR_B_PWM);
             }else{
-                LATC &= ~(1<<MOTOR_A_PWM);
+                OC1RS = 0;
+                OC2RS = 0;
+                /*LATC &= ~(1<<MOTOR_A_PWM);
                 LATC &= ~(1<<MOTOR_B_PWM);
                 LATC &= ~(1<<MOTOR_A_DIR);
-                LATC &= ~(1<<MOTOR_B_DIR);
+                LATC &= ~(1<<MOTOR_B_DIR);*/
             }
           }
         else if(c == 'B'){      //move reverse (all motors rotate in reverse direction)
-            LATC |= (1<<MOTOR_A_PWM);
-            LATC |= (1<<MOTOR_B_PWM);
+            OC1RS = PERIODO * factor_servicio;
+            OC2RS = PERIODO * factor_servicio;
+            /*LATC |= (1<<MOTOR_A_PWM);
+            LATC |= (1<<MOTOR_B_PWM);*/
             LATC |= (1<<MOTOR_A_DIR);
             LATC |= (1<<MOTOR_B_DIR);
           }
 
         else if(c == 'L'){      //turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
-            LATC |= (1<<MOTOR_A_PWM);
-            LATC |= (1<<MOTOR_B_PWM);
+            OC1RS = PERIODO * factor_servicio;
+            OC2RS = PERIODO * factor_servicio;
+            /*LATC |= (1<<MOTOR_A_PWM);
+            LATC |= (1<<MOTOR_B_PWM);*/
             LATC |= (1<<MOTOR_B_DIR);
           }
 
         else if(c == 'R'){      //turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
-            LATC |= (1<<MOTOR_A_PWM);
-            LATC |= (1<<MOTOR_B_PWM);
+            OC1RS = PERIODO * factor_servicio;
+            OC2RS = PERIODO * factor_servicio;
+            /*LATC |= (1<<MOTOR_A_PWM);
+            LATC |= (1<<MOTOR_B_PWM);*/
             LATC |= (1<<MOTOR_A_DIR);
           }
 
@@ -118,8 +183,10 @@ void modificarCoche(char c, float distancia){
         
 
         else if(c == 'S'){      //STOP (all motors stop)
-            LATC &= ~(1<<MOTOR_A_PWM);
-            LATC &= ~(1<<MOTOR_B_PWM);
+            OC1RS = 0;
+            OC2RS = 0;
+            /*LATC &= ~(1<<MOTOR_A_PWM);
+            LATC &= ~(1<<MOTOR_B_PWM);*/
             LATC &= ~(1<<MOTOR_A_DIR);
             LATC &= ~(1<<MOTOR_B_DIR);
           }
